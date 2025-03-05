e79786dcc4d9cd0cbae0d4582617abbd2f1a1fac
Jon Owens <15575425+DragoQCC@users.noreply.github.com>
2025-03-05 10:22:38 -0500

    Update of the Message Progress Class

--- AvionRelay.Core/Features/Messages/Models/MessageProgress.cs
+++ AvionRelay.Core/Features/Messages/Models/MessageProgress.cs
@@ -5,5 +5,39 @@
 /// </summary>
 public class MessageProgress
 {
+    internal MessageState? State { get; set; } = null;
+
+    public MessageProgress(MessageState state)
+    {
+        this.ChangeStateTo(state);
+    }
+
+    public void ChangeStateTo(MessageState state)
+    {
+        this.State?.OnExit();
+        this.State = state;
+        this.State.SetContext(this);
+        this.State?.OnEnter();
+    }
     
+}
+
+// An IMessageState Interface, I want to use the state pattern here
+public abstract class MessageState
+{
+    protected MessageProgress ProgressContext { get; set; }
+
+    public MessageState(MessageProgress progressContext)
+    {
+        ProgressContext = progressContext;
+    }
+
+    internal void SetContext(MessageProgress progressContext)
+    {
+        this.ProgressContext = progressContext;
+    }
+
+    public abstract void OnEnter();
+    public abstract void OnExit();
+
 }
